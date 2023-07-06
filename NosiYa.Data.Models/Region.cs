0d104c2c3edde22c40d5d77aafb0e9f939fe670a
TabakovG <gnt137@gmail.com>
2023-07-06 23:46:08 +0300

    Signed-off-by: TabakovG <gnt137@gmail.com>

--- NosiYa.Data.Models/Region.cs
+++ NosiYa.Data.Models/Region.cs
@@ -7,6 +7,7 @@
             this.Outfits = new HashSet<Outfit.Outfit>();
         }
         public int RegionId { get; set;}
+
         public string RegionName { get; set; } = null!; 
 
         public ICollection<Outfit.Outfit> Outfits { get; set;}
