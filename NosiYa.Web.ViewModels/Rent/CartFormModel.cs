0ce3d29d49335bb52c301c6496f0bdc7dd315bd2
TabakovG <gnt137@gmail.com>
2023-07-27 01:49:50 +0300

    rent outfit calendar date picker configured

--- NosiYa.Web.ViewModels/Rent/CartFormModel.cs
+++ NosiYa.Web.ViewModels/Rent/CartFormModel.cs
@@ -1,17 +1,23 @@
-﻿using NosiYa.Web.ViewModels.OutfitSet;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
+using NosiYa.Web.ViewModels.OutfitSet;
 
 namespace NosiYa.Web.ViewModels.Rent
 {
 	public class CartFormModel
 	{
+
+		[Required]
+		public int CartId { get; set; }
+
 		public OutfitSetForRentViewModel OutfitModel { get; set; } = null!;
 
+		[Required]
 		public DateTime FromDate { get; set; }
 
+		[Required]
 		public DateTime ToDate { get; set; }
 
 		public ICollection<string> ReservedDates { get; set; } = new HashSet<string>();
-
-		public int CartId { get; set; }
 	}
 }
